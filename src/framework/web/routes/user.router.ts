e53f61a9f1c2d59c7646e34cdc990018e2647105
Asuka <asuka@>
2023-10-27 14:39:50 +0300

    JSdoc user routes update

--- src/framework/web/routes/user.router.ts
+++ src/framework/web/routes/user.router.ts
@@ -8,10 +8,198 @@ export const userRouter = (dependencies: IDependency) => {
   const userController = userControllerCreate(dependencies);
 
   router.use(appInit);
-
+  /**
+   * @swagger
+   * /users:
+   *   get:
+   *     summary: Возвращает данные пользователя на основании JWT токера, содержащегося в заголовке запроса.
+   *     tags:
+   *       - users
+   *     responses:
+   *       '200':    
+   *          description: Возвращает объект с информацией пользователя.
+   *          content:
+   *            application/json:
+   *              schema: 
+   *                type: object
+   *                properties:
+   *                  status:
+   *                    type: string
+   *                    example: success
+   *                  data:
+   *                    type: object
+   *                    properties:
+   *                      user:
+   *                        type: object   
+   *       '500':    
+   *          description: Возвращает описание ошибки
+   *          content:
+   *            application/json:
+   *              schema: 
+   *                type: object
+   *                properties:
+   *                  status:
+   *                    type: string
+   *                    example: error
+   *                  message:
+   *                    type: string
+   *                    example: Server error
+   */
   router.get('/', userController.getController);
+  /**
+   * @swagger
+   * /users/signup:
+   *   post:
+   *     summary: Создание нового пользователя
+   *     tags:
+   *       - users
+   *     requestBody:
+   *       required: true
+   *       content:
+   *         application/json:
+   *           schema:
+   *             type: object
+   *             properties:
+   *               username: 
+   *                 type: string
+   *               password: 
+   *                 type: string
+   *     responses:
+   *       '200':    
+   *          description: Возвращает объект с информацией пользователя и токены доступа.
+   *          content:
+   *            application/json:
+   *              schema: 
+   *                type: object
+   *                properties:
+   *                  status:
+   *                    type: string
+   *                    example: success
+   *                  data:
+   *                    type: object
+   *                    properties:
+   *                      user:
+   *                        type: object   
+   *                      accessToken:
+   *                        type: string  
+   *                      refreshToken:
+   *                        type: string  
+   *       '500':    
+   *          description: Возвращает описание ошибки
+   *          content:
+   *            application/json:
+   *              schema: 
+   *                type: object
+   *                properties:
+   *                  status:
+   *                    type: string
+   *                    example: error
+   *                  message:
+   *                    type: string
+   *                    example: Server error
+   */
   router.post('/signup', userController.createController);
+  /**
+   * @swagger
+   * /users/signip:
+   *   post:
+   *     summary: Возвращает пользователя на основании имени пользователя и пароля
+   *     tags:
+   *       - users
+   *     requestBody:
+   *       required: true
+   *       content:
+   *         application/json:
+   *           schema:
+   *             type: object
+   *             properties:
+   *               username: 
+   *                 type: string
+   *               password: 
+   *                 type: string
+   *     responses:
+   *       '200':    
+   *          description: Возвращает объект с информацией пользователя и токены доступа.
+   *          content:
+   *            application/json:
+   *              schema: 
+   *                type: object
+   *                properties:
+   *                  status:
+   *                    type: string
+   *                    example: success
+   *                  data:
+   *                    type: object
+   *                    properties:
+   *                      user:
+   *                        type: object   
+   *                      accessToken:
+   *                        type: string  
+   *                      refreshToken:
+   *                        type: string  
+   *       '500':    
+   *          description: Возвращает описание ошибки
+   *          content:
+   *            application/json:
+   *              schema: 
+   *                type: object
+   *                properties:
+   *                  status:
+   *                    type: string
+   *                    example: error
+   *                  message:
+   *                    type: string
+   *                    example: Server error
+   */
   router.post('/signin', userController.signinController);
+  /**
+   * @swagger
+   * /users/signip:
+   *   post:
+   *     summary: Обновляет токены пользователя.
+   *     tags:
+   *       - users
+   *     requestBody:
+   *       required: true
+   *       content:
+   *         application/json:
+   *           schema:
+   *             type: object
+   *             properties:
+   *               refreshToken: 
+   *                 type: string
+   *     responses:
+   *       '200':    
+   *          description: Возвращает новые токены доступа.
+   *          content:
+   *            application/json:
+   *              schema: 
+   *                type: object
+   *                properties:
+   *                  status:
+   *                    type: string
+   *                    example: success
+   *                  data:
+   *                    type: object
+   *                    properties:
+   *                      accessToken:
+   *                        type: string  
+   *                      refreshToken:
+   *                        type: string  
+   *       '500':    
+   *          description: Возвращает описание ошибки
+   *          content:
+   *            application/json:
+   *              schema: 
+   *                type: object
+   *                properties:
+   *                  status:
+   *                    type: string
+   *                    example: error
+   *                  message:
+   *                    type: string
+   *                    example: Server error
+   */
   router.post('/token', userController.refreshTokenController);
   return router;
 }
